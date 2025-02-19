8501791b7e0672719b640c36bda17a72aef6d01d
GabrielMarie <118224982+GabrielMarie@users.noreply.github.com>
2025-02-19 17:18:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text()) // Récupère le contenu HTML du fichier
+        .then(data => {
+            document.getElementById("content").innerHTML = data; // Insère le contenu dans la page
+        })
+        .catch(error => console.error('Erreur de chargement :', error));
+}
 
