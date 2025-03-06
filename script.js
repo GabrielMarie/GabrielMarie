4c682374463a68f3301d3979ab887675456d9d78
GabrielMarie <gabriel.saint.gelais@gmail.com>
2025-03-05 21:43:20 -0500

    teste

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ function loadPage(page) {
         })
         .catch(error => console.error('Erreur de chargement :', error));
 }
+
+//animation de bannière
 document.addEventListener("DOMContentLoaded", function () {
     let banner = document.getElementById("siteBanner");
     let accueil = window.location.pathname.includes("index.html") || window.location.pathname === "GabrielMarie/";
@@ -25,4 +27,28 @@ document.addEventListener("DOMContentLoaded", function () {
         banner.src = "le site de gab fin.gif";
     }
 });
+
+//latex
+function loadPage(page) {
+    fetch(page)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("content").innerHTML = data;
+        
+        // Vérifier si MathJax est chargé et forcer le rendu
+        if (window.MathJax) {
+            MathJax.typesetPromise();
+        }
+    });
+}
+
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('main').innerHTML = data;
+            history.pushState(null, '', page); // Met à jour l’URL
+        })
+        .catch(error => console.error('Erreur de chargement:', error));
+}
 
