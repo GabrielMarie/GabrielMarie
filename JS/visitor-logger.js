d18c0e7494e37d1aac09d648ac6fbf78012922b9
GabrielMarie <118224982+GabrielMarie@users.noreply.github.com>
2026-05-28 15:33:25 -0400

    g

--- JS/visitor-logger.js
+++ JS/visitor-logger.js
@@ -1,13 +1,8 @@
-
-(async function () {
-  try {
-    await fetch(
-      "https://visitor-traker.gabriel-saint-gelais.workers.dev/collect?page=" +
-      encodeURIComponent(location.pathname),
-      {
-        method: "GET",
-        mode: "cors"
-      }
-    );
-  } catch (e) {}
+(function () {
+  const endpoint = 'https://visitor-traker.gabriel-saint-gelais.workers.dev/collect?page=' + encodeURIComponent(location.pathname);
+  fetch(endpoint, {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include'
+  }).catch(() => {});
 })();
